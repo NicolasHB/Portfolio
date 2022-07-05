6f6506a380779d3d999ad20367c4691e5bbe5ff5
NicolasHB <brierenicolas0@gmail.com>
2022-07-05 16:48:27 +0200

    CardProfileLogo

--- src/Components/CardProfil.js
+++ src/Components/CardProfil.js
@@ -3,14 +3,84 @@ import React from 'react'
 export default function Card() {
   return (
     <div className="p-5 ">
-      <img className='max-w-xs mr-0 col-start-3""' src="xsmith.jpg.pagespeed.ic.-fvXWI23nq.webp" alt="profil" />
-      <div className="bg-blue-800 max-w-xs">
-        <h1>General Information</h1>
-        <p>Name: </p>
-        <p>Location:</p>
-        <p>mail</p>
-        <p>phone</p>
-        <p>site/github</p>
+      <img
+        className='max-w-xs mr-0 col-start-3""'
+        src="xsmith.jpg.pagespeed.ic.-fvXWI23nq.webp"
+        alt="profil"
+      />
+      <div className="bg-blue-800 max-w-xs pl-10">
+        <h1 className="font-black text-center text-white">
+          General Information
+        </h1>
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+            <path
+              fill-rule="evenodd"
+              d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <p>Name: </p>
+        </div>
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <p>Location:</p>
+        </div>
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+          </svg>
+          <p>mail:</p>
+        </div>
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+          </svg>
+          <p>phone</p>
+        </div>
+        <div className="flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14.243 5.757a6 6 0 10-.986 9.284 1 1 0 111.087 1.678A8 8 0 1118 10a3 3 0 01-4.8 2.401A4 4 0 1114 10a1 1 0 102 0c0-1.537-.586-3.07-1.757-4.243zM12 10a2 2 0 10-4 0 2 2 0 004 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <p>site/github</p>
+        </div>
       </div>
     </div>
   );
