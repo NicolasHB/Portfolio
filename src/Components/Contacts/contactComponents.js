85481cd6a172c2a301856b2079ab9dfbbd55d14c
NicolasHB <brierenicolas0@gmail.com>
2022-07-05 15:45:20 +0200

    formulaire retouche

--- src/Components/Contacts/contactComponents.js
+++ src/Components/Contacts/contactComponents.js
@@ -6,13 +6,13 @@ export default function ContactComponents() {
     <div className="bg-gray-50 p-5 mr-10">
       <div className="">
         <h1 className="text-6xl font-extrabold pb-10">Contactez moi</h1>
-        <p>
+        <p className=''>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
           rem. In officiis, alias quis ea quaerat veniam accusantium facere quae
           minus, perferendis odio nostrum eos!
         </p>
       </div>
-      <div className="">
+      <div className="pt-3">
         <ContactFormulaire />
         <div className="pt-5">
           <button className=" bg-blue-800 text-white ">
